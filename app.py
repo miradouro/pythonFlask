d89c0937cdd3ddbfbe4c09bb48492186132284dc
Rafael <you@example.com>
2022-10-09 18:41:40 -0300

    aula 24 completa

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, flash
+from flask_sqlalchemy import SQLAlchemy
 import urllib.request
 import json
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'anystring'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///cursos.sqlite3'
+
+db = SQLAlchemy(app)
 
 frutas = []
 registros = []
+
+
+class cursos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50))
+    descricao = db.Column(db.String(100))
+    ch = db.Column(db.Integer)
+
+    def __init__(self, nome, descricao, ch):
+        self.nome = nome
+        self.descricao = descricao
+        self.ch = ch
 
 
 @app.route('/', methods=["GET", "POST"])
@@ -46,6 +63,29 @@ def filmes(propriedade):
     return render_template("filmes.html", filmes=jsondata['results'])
 
 
+@app.route('/cursos')
+def lista_cursos():
+    return render_template("cursos.html", cursos=cursos.query.all())
+
+
+@app.route('/cria_curso', methods=["GET", "POST"])
+def cria_curso():
+    nome = request.form.get('nome')
+    descricao = request.form.get('descricao')
+    ch = request.form.get('ch')
+    if request.method == 'POST':
+        if not nome or not descricao or not ch:
+            flash("Preencha todos os campos do formulario", "error")
+        else:
+            curso = cursos(nome, descricao, ch)
+            db.session.add(curso)
+            db.session.commit()
+            return redirect(url_for('lista_cursos'))
+    return render_template("novo_curso.html")
+
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
 
